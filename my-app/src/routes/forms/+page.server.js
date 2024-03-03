74a5f81401e3c47ad6bdb87590c238025b9e85f4
unknown <colinmac1004@gmail.com>
2024-03-03 17:34:25 -0500

    changed the get call to have a query param

--- my-app/src/routes/forms/+page.server.js
+++ my-app/src/routes/forms/+page.server.js
@@ -2,7 +2,12 @@ import { redirect } from '@sveltejs/kit';
 
 // @ts-ignore
 export const load = async ({ cookies }) => {
-    const response = await fetch('http://localhost:1323/restricted', { headers: { 'Authorization': cookies.get('Auth-token') } })
+    // const response = await fetch('http://localhost:1323/restricted', { headers: { 'Authorization': cookies.get('Auth-token') } })
+
+    const response = await fetch('http://localhost:1323/inventory/products/yak', {
+        headers: { 'Authorization': cookies.get('Auth-token') }
+    })
+    console.log(await response.json())
 
     if (response.status !== 200) {
         throw redirect(302, "/login");
